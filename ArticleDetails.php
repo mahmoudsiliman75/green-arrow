1c52bb2d64bf8080619a02ebab466bc7d3d85648
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-02-22 10:44:58 +0200

    finish article details page

--- ArticleDetails.php
+++ ArticleDetails.php
@@ -32,6 +32,13 @@
             <span class="inner-badge"> 20, Jan </span> 
           </span>
           <img src="assets/pics/pic22.jpg" alt="Product Details Img" class="img-fluid">
+          <h4 class="d-flex justify-content-between mt-3"> 
+            Article Title
+            <span>
+              <i class="fas fa-bookmark"></i>
+              Related Service
+            </span> 
+          </h4>
         </div>
       </div>
     </div>
@@ -41,7 +48,6 @@
     <div class="row">
       <div class="col-12">
         <div class="article-details-text">
-          <h2 class="sec-heading mb-4"> Article Name </h2>
           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta neque, facere quae cum quis explicabo delectus velit ut? Reiciendis facilis adipisci dolorum aspernatur modi corporis quod fugit ullam asperiores quisquam. 
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta neque, facere quae cum quis explicabo delectus velit ut? Reiciendis facilis adipisci dolorum aspernatur modi corporis quod fugit ullam asperiores quisquam. 
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta neque, facere quae cum quis explicabo delectus velit ut? Reiciendis facilis adipisci dolorum aspernatur modi corporis quod fugit ullam asperiores quisquam. 
@@ -53,56 +59,75 @@
 </div>
 <!-- START:: DETAILS SECTION -->
 
-<!-- START:: VIDEOS SECTION -->
-<div class="rel-videos">
+<!-- START:: NEWS SECTION -->
+<div class="news">
   <div class="container">
-
-    <h2 class="sec-heading mb-5"> Related Videos </h2>
-
-    <div id="videos-slider" class="owl-carousel owl-theme">
-      <div class="videos-slider-item"> 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/-jwSrDCd-vM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <h4 class="text-center"> Video Title </h4>
-      </div>
-
-      <div class="videos-slider-item"> 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/-jwSrDCd-vM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <h4 class="text-center"> Video Title </h4>
-      </div>
-
-      <div class="videos-slider-item"> 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/-jwSrDCd-vM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <h4 class="text-center"> Video Title </h4>
-      </div>
-
-      <div class="videos-slider-item"> 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/-jwSrDCd-vM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <h4 class="text-center"> Video Title </h4>
-      </div>
-
-      <div class="videos-slider-item"> 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/-jwSrDCd-vM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <h4 class="text-center"> Video Title </h4>
-      </div>
-
-      <div class="videos-slider-item"> 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/-jwSrDCd-vM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <h4 class="text-center"> Video Title </h4>
-      </div>
-
-      <div class="videos-slider-item"> 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/-jwSrDCd-vM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <h4 class="text-center"> Video Title </h4>
-      </div>
+    <div class="d-flex justify-content-center">
+      <h2 class="sec-heading mt-5"> Related Services </h2>
     </div>
+    <div class="row justify-content-center">
 
-    <div class="col-12 text-center mt-5">
-      <?php $link = 'VideosLiberary.php'; $title = 'View More' ?>
-      <?php include(INC.'/ui/MainBtn.php')?>
+      <div class="col-12 col-md-6 col-lg-4 my-3 p-2">
+        <div class="card">
+          <span class="badge"> 
+            <span class="inner-badge"> 20, Jan </span> 
+          </span>
+          <a href="ArticleDetails.php">
+            <img src="assets/pics/pic22.jpg" class="card-img-top" alt="...">
+          </a>
+          <div class="card-body text-center">
+            <h4 class="card-title mb-3 mt-2">Spirulina</h4>
+            <p class="card-text mb-4"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium consequatur itaque voluptatibus non. Impedit voluptas tempora assumenda voluptatem autem nostrum, reprehenderit iusto, non, expedita dolores distinctio nisi unde sed velit. </p>
+
+            <!-- ArticleDetails.php -->
+            <?php $link = 'ArticleDetails.php'; $title = 'More Details' ?>
+            <?php include(INC.'/ui/MainBtn.php')?>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-4 my-3 p-2">
+        <div class="card">
+          <span class="badge"> 
+            <span class="inner-badge"> 20, Jan </span> 
+          </span>
+          <a href="ArticleDetails.php">
+            <img src="assets/pics/pic1.jpg" class="card-img-top" alt="...">
+          </a>
+          <div class="card-body text-center">
+            <h4 class="card-title mb-3 mt-2">Spirulina</h4>
+            <p class="card-text mb-4"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium consequatur itaque voluptatibus non. Impedit voluptas tempora assumenda voluptatem autem nostrum, reprehenderit iusto, non, expedita dolores distinctio nisi unde sed velit. </p>
+
+            <!-- ArticleDetails.php -->
+            <?php $link = 'ArticleDetails.php'; $title = 'More Details' ?>
+            <?php include(INC.'/ui/MainBtn.php')?>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-4 my-3 p-2">
+        <div class="card">
+          <span class="badge"> 
+            <span class="inner-badge"> 20, Jan </span> 
+          </span>
+          <a href="ArticleDetails.php">
+            <img src="assets/pics/pic3.jpg" class="card-img-top" alt="...">
+          </a>
+          <div class="card-body text-center">
+            <h4 class="card-title mb-3 mt-2">Spirulina</h4>
+            <p class="card-text mb-4"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium consequatur itaque voluptatibus non. Impedit voluptas tempora assumenda voluptatem autem nostrum, reprehenderit iusto, non, expedita dolores distinctio nisi unde sed velit. </p>
+
+            <!-- ArticleDetails.php -->
+            <?php $link = 'ArticleDetails.php'; $title = 'More Details' ?>
+            <?php include(INC.'/ui/MainBtn.php')?>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </div>
-<!-- START:: VIDEOS SECTION -->
+<!-- END:: NEWS SECTION -->
 
 
 
